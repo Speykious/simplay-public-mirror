84661226e878407f819be8da130507b749bc866e
Oglo12 <ogloooglooagla@gmail.com>
2023-08-30 10:40:46 -0500

    More work on the internal server.

--- src/server.rs
+++ src/server.rs
@@ -2,11 +2,33 @@
 
 use bevy::prelude::*;
 
+#[derive(Debug, PartialEq)]
+pub enum SMError {
+    UnexpectedServerValidation, // This is basically a fallback.
+    IllegalMove,
+    IllegalChunkLoad,
+    IllegalBlockBreak,
+    IllegalHitPoint,
+    SubjectTriedToFly,
+    InvalidPlayer,
+    IllegalItemOperation,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SMValid {
+    Yes,
+    No(SMError),
+}
+
 #[derive(Resource)]
 pub struct InternalServer {}
 
 impl InternalServer {
     pub fn new() -> Self {
         return Self {};
+    }
+
+    pub fn ask() -> SMValid {
+        todo!();
     }
 }
