d187016cf4a2c6e81ee69459e55f8ff0862b55f2
Oglo12 <ogloooglooagla@gmail.com>
2023-10-23 21:33:37 -0500

    Checkpoint.

--- src/chunk_manager.rs
+++ src/chunk_manager.rs
@@ -1,10 +1,55 @@
 #![allow(dead_code)]
 
 use bevy::prelude::*;
-use hashbrown::HashMap;
+
+use crate::chunk::*;
+use crate::block::*;
+use crate::places;
 
 pub struct ChunkManagerPlugin;
 
 impl Plugin for ChunkManagerPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, test_chunks);
+    }
+}
+
+fn test_chunks(
+    mut cmds: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    asset_server: ResMut<AssetServer>,
+) {
+    for cx in 0..2 {
+        for cy in 0..2 {
+            for cz in 0..2 {
+                let mut chunk = Chunk::new((cx, cy, cz));
+
+                for x in 0..CHUNK_SIZE.0 {
+                    for y in 0..CHUNK_SIZE.1 {
+                        for z in 0..CHUNK_SIZE.2 {
+                            chunk.set_block_u8((x, y, z), BlockType::Dirt);
+                        }
+                    }
+                }
+
+                cmds.spawn((
+                    PbrBundle {
+                        mesh: meshes.add(chunk.mesh()),
+                        transform: Transform::from_xyz((cx * CHUNK_SIZE.0 as isize) as f32, (cy * CHUNK_SIZE.1 as isize) as f32, (cz * CHUNK_SIZE.2 as isize) as f32),
+                        material: materials.add(StandardMaterial {
+                            // base_color: Color::rgb(0.05, 0.5, 0.35),
+                            base_color_texture: Some(asset_server.load(format!("{}/block_atlas.png", places::custom_built_assets().to_string()))),
+                            // double_sided: true, // debug
+                            // cull_mode: None, // debug
+                            reflectance: 0.15,
+                            perceptual_roughness: 0.6,
+                            ..default()
+                        }),
+                        ..default()
+                    }, Name::new(format!("Chunk ({}, {}, {})", cx, cy, cz))
+                ));
+            }
+        }
+    }
 }
